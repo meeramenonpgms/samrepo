4347d17edb94f3de7ed8bd9062422b5cca95209e
meeramenonpgms <admin@Admins-MacBook-Pro.local>
2020-06-03 19:39:51 +0530

    last changes

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express=require('express');
+var bodyparser=require("body-parser")
 const server=express();
+var data=[
+    {Name:"Ram",Age:12,Sub:"Maths"},
+    {Name:"John",Age:13,Sub:"chemistry"},
+    {Name:"smith",Age:14,Sub:"botany"}];
+server.use(bodyparser.urlencoded({extended:true}))
+
 server.set("view engine","ejs");
+
 server.set("views","./pages")
+
 server.get("/",function(req,res){
    res.render("login")
 })
 server.get("/home",function(req,res){
-    res.render("home")
+    res.render("home",)
 })
 server.get("/aboutus",function(req,res){
     res.render("aboutus")
@@ -18,7 +27,17 @@ server.get("/login",function(req,res){
     res.send("login page")
 })
 server.post("/validate",function(req,res){
-    res.render("home")
+    var user=req.body.userid;
+    console.log(user)
+    var password=req.body.pass;
+    console.log(password)
+    if(user=="admin"&&password=="pass@123"){
+        res.render("home",{students:data})
+    }
+    else{
+        res.render("login")
+    }
+   
 })
 server.post("/check",function(req,res){
     
